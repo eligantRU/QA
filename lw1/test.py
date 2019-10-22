2d1446ad2f3db8ccf608ca450d5189921b10d19c
Nick Yegorov <eligant.ru@gmail.com>
2019-10-23 02:59:06 +0300

    Hotfix

--- lw1/test.py
+++ lw1/test.py
@@ -12,8 +12,8 @@ def parse_command_line_arguments():
     return args.target_file_name, args.test_cases_file_name
 
 
-def get_formatted_comparison_result(result, line_index):
-    return f'{line_index}[{3 * line_index - 2}]: {"Success" if result else "Failed"}'
+def get_formatted_comparison_result(result, line_index, args):
+    return f'{line_index}[{3 * line_index - 2}]: {"Success" if result else "Failed"} {args}'
 
 
 def main():
@@ -26,7 +26,7 @@ def main():
         line_indexes = [index for index in range(1, 1 + ceil((len(lines)) / 3))]
         for args, expected_output, line_index in zip(command_line_arguments, expected_outputs, line_indexes):
             output = check_output(["python", target_file_name] + args).strip()
-            print(get_formatted_comparison_result(output.decode("utf-8") == expected_output, line_index))
+            print(get_formatted_comparison_result(output.decode("utf-8") == expected_output, line_index, args))
 
 
 if __name__ == "__main__":
